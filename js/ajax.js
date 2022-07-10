b0eb0221a030c8f75fac4ec9740aaccebd780678
cwscrsj <2354554898@qq.com>
2022-07-10 17:04:42 +0800

    图片加载demo

--- js/ajax.js
+++ js/ajax.js
@@ -18,70 +18,70 @@ function objToFormdata(obj) {
 
 
 
-function myAjax(options) {
-    let promise = new Promise((resolve, reject) => {
-        // 不需要发送值则让data置空
-        if (!options.data) options.data = '';
-        if (!options.type) options.type = '';
+async function myAjax(options) {
+    try {
+        let promise = await new Promise((resolve, reject) => {
+            // 不需要发送值则让data置空
+            if (!options.data) options.data = '';
+            if (!options.type) options.type = '';
 
-        let qs = null;
-        let xhr = new XMLHttpRequest();
+            let qs = null;
+            let xhr = new XMLHttpRequest();
 
-        // 先转换为大写形式，方便判断
-        let method = options.method.toUpperCase();
-        let type = options.type.toUpperCase();
+            // 先转换为大写形式，方便判断
+            let method = options.method.toUpperCase();
+            let type = options.type.toUpperCase();
 
-        //处理传输数据格式
-        switch (type) {
-            case 'JSON':
-                qs = JSON.stringify(options.data);
-                break;
-            case 'FORMDATA':
-                // 判断传入的是表单还是对象
-                if (options.dataType && options.dataType == 'form')
-                    qs = new FormData(options.data);
+            //处理传输数据格式
+            switch (type) {
+                case 'JSON':
+                    qs = JSON.stringify(options.data);
+                    break;
+                case 'FORMDATA':
+                    // 判断传入的是表单还是对象
+                    if (options.dataType && options.dataType == 'form')
+                        qs = new FormData(options.data);
 
-                else
-                    qs = objToFormdata(options.data);
+                    else
+                        qs = objToFormdata(options.data);
 
-            default:
-                qs = resolveData(options.data);
-        };
-        //判断是post还是Get
-        switch (method) {
-            case 'POST':
-                xhr.open(method, options.url);
-                // 如果是json格式则更换一下请求头
-                if (type == 'JSON')
-                    xhr.setRequestHeader('Content-Type', 'application/json');
-                xhr.send(qs);
-                break;
-            case 'GET':
-                xhr.open(method, options.url + '?' + qs);
-                // xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-                xhr.send();
+                default:
+                    qs = resolveData(options.data);
+            };
+            //判断是post还是Get
+            switch (method) {
+                case 'POST':
+                    xhr.open(method, options.url);
+                    // 如果是json格式则更换一下请求头
+                    if (type == 'JSON')
+                        xhr.setRequestHeader('Content-Type', 'application/json');
+                    xhr.send(qs);
+                    break;
+                case 'GET':
+                    xhr.open(method, options.url + '?' + qs);
+                    // xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+                    xhr.send();
 
-                break;
-        };
+                    break;
+            };
 
-        xhr.onreadystatechange = function () {
-            if (xhr.readyState === 4) {
-                if ((xhr.status >= 200 && xhr.status < 300) || xhr.status === 304) {
-                    let result = JSON.parse(xhr.responseText);
-                    resolve(result);
-                } else {
-                    reject(new Error("error"))
+            xhr.onreadystatechange = function () {
+                if (xhr.readyState === 4) {
+                    if ((xhr.status >= 200 && xhr.status < 300) || xhr.status === 304) {
+                        let result = JSON.parse(xhr.responseText);
+                        resolve(result);
+                    } else {
+                        reject(new Error("error"))
+                    }
                 }
             }
-        }
 
 
-    })
+        })
+        return promise;
+    } catch {
+        console.log(2);
+    }
 
-    return promise
 }
 
-
-
-export { myAjax, objToFormdata, resolveData };
-// 封装一个登录查询的函数
