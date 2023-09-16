2f3441a22128ddb03befdb42e1a7135c883c05d0
eli <2354554898@qq.com>
2023-09-16 20:10:44 +0800

    feat: 添加强制缓存demo

--- cacheDemo/src/cache.route.js
+++ cacheDemo/src/cache.route.js
@@ -10,7 +10,7 @@ assetRouter.get("/", async (ctx) => {
 assetRouter.get("/txt", async (ctx) => {
   const getResource = () => {
     return new Promise((res) => {
-      fs.readFile("./assets/testcache.txt", "utf8", (err, data) => {
+      fs.readFile("./public/testcache.txt", "utf8", (err, data) => {
         if (err) {
           return;
         }
@@ -18,15 +18,38 @@ assetRouter.get("/txt", async (ctx) => {
       });
     });
   };
-  ctx.response.set("Cache-Control", "public, max-age=10"); //设置强缓存，过期时间为10秒
+
+  ctx.response.set("Cache-Control", "public, max-age=3000"); //设置强缓存，过期时间为10秒
   ctx.response.set("Content-Type", "text/plain");
   // ctx.response.set("Content-Type", "text/plain")
   ctx.body = await getResource();
 });
 
-// 查看文本
+// 修改文本
+assetRouter.post("/txt", async (ctx) => {
+  const body = ctx.request.body;
+  function writeToFile(file, line) {
+    return new Promise((resolve, reject) => {
+      // 添加新的一行
+      const newLine = "\n" + line;
+
+      // 将修改后的文本写回到文件中
+      fs.writeFile(file, newLine, { flag: "a" }, (err) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve();
+        }
+      });
+    });
+  }
+  await writeToFile("./public/testcache.txt", `\n${body.data}`);
+  ctx.body = "写入成功";
+});
+
+// 查看照片
 assetRouter.get("/img", async (ctx) => {
-  ctx.response.set("Cache-Control", "max-age=100"); //设置强缓存，过期时间为10秒
+  // ctx.response.set("Cache-Control", "max-age=100"); //设置强缓存，过期时间为10秒
   ctx.response.set("Content-Type", "image/jpg");
   // ctx.response.set("Content-Type", "text/plain")
   ctx.body = await getResource();
--- cacheDemo/src/index.js
+++ cacheDemo/src/index.js
@@ -2,10 +2,14 @@ const Koa = require("koa");
 const static = require("koa-static");
 const path = require("path");
 const publicPath = path.join(__dirname, "./../public");
-console.log(static(publicPath));
+const bodyParser = require("koa-bodyparser");
+
 const app = new Koa();
 const assetRouter = require(`./cache.route.js`);
+const cors = require("@koa/cors");
 
+app.use(cors());
+app.use(bodyParser());
 app.use(assetRouter.routes());
 app.use(assetRouter.allowedMethods());
 
